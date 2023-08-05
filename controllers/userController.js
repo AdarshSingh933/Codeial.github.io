96eab9403ef0e0dc2c4c16e640beb467373155bc
AdarshSingh933 <130076154+AdarshSingh933@users.noreply.github.com>
2023-08-05 12:52:38 +0530

    Sign-Up done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,57 @@
+const User = require('../models/user');
+
 module.exports.profile= function(req,res){
     return res.render('user_profile',{
         title:'Profile'
     });
+}
+// module.exports.signUp =async function(req,res){
+//     try{
+//           await User.create({
+//             email:req.body.email,
+//             password:req.body.password,
+//             name:req.body.name
+//           });
+//     }catch(err){
+//         console.log("error",err);
+//     }
+//     return res.redirect('/signIn');
+// }
+// module.exports.singIn =async function(req,res){
+//     const email = req.body.email;
+//     const password = req.body.password;
+//     const user = User.find({email,password});
+//     if(user){
+//         return res.redirect('/profile');
+//     }else{
+//         return ;
+//     } 
+// }
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title: "Codeial | Sign Up"
+    });
+}
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title: "Codeial | Sign In"
+    });
+}
+module.exports.create = async function(req,res){
+    if(req.body.password !== req.body['confirm-password']){
+        return res.redirect('back');
+    }
+    try{
+        const user = await User.findOne({email:req.body.email});
+        if(!user){
+            await User.create(req.body);
+            return res.redirect('/user/sign-in');
+        }else{
+             return res.redirect('back');
+        }
+    }catch(err){
+        console.log("error",err);
+        return ;
+    }
+    
 }
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+const cookieParser = require('cookie-parser');
 const app=express();
 
+app.use(express.urlencoded());
+app.use(cookieParser());
 app.use(expressLayouts);
 app.use(express.static('./assets'));
 
